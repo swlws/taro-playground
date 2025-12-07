394000cbcb3f2326905f3196a7dee277c68e72cc
swlws <979623074@qq.com>
2025-12-08 07:44:55 +0800

    feat: CustomWrapper

--- src/pages/demo-v-list/components/row/index.tsx
+++ src/pages/demo-v-list/components/row/index.tsx
@@ -1,25 +1,22 @@
-
-import { View, Image, Text } from "@tarojs/components";
+import { View, Image, Text, CustomWrapper } from "@tarojs/components";
 import { memo } from "react";
 
-type Item = {
-  id: string;
-  title: string;
-  time: string;
-  img: string;
-};
-
 function Row({ id, index, style, data }: any) {
-  const it: Item = data[index];
+  const it = data[index];
   return (
-    <View id={id} className="vlist__item" style={style}>
-      <Image className="vlist__item-img" src={it.img} mode="aspectFill" lazyLoad />
-      <View className="vlist__item-body">
-        <Text className="vlist__item-title">{it.title}</Text>
-        <Text className="vlist__item-time">{it.time}</Text>
-      </View>
+    <View id={id} style={style}>
+      <CustomWrapper>
+        {/* 这是“真实渲染节点”，把类放这里 */}
+        <View className="vlist__item">
+          <Image className="vlist__item-img" src={it.img} mode="aspectFill" lazyLoad />
+          <View className="vlist__item-body">
+            <Text className="vlist__item-title">{it.title}</Text>
+            <Text className="vlist__item-time">{it.time}</Text>
+          </View>
+        </View>
+      </CustomWrapper>
     </View>
   );
 }
 
-export default memo(Row);  
+export default memo(Row);
